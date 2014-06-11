507213807c97462d406392d859925efef94ae426
jlamimoso <jlamimoso@10.0.1.10>
2014-06-11 00:37:38 -0300

    Alteracao numero 1

--- src/cliente.py
+++ src/cliente.py
@@ -10,7 +10,7 @@ print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
 while True:
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
